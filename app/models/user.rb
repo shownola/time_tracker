9011924e9649fc488c6683c5ae6f62da49c01d14
SHERRY <swasieko@gmail.com>
2017-01-08 03:33:01 +0000

    Implemented user connection to posts with test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,6 @@ class User < ActiveRecord::Base
          
          
   validates_presence_of :first_name, :last_name
+  
+  
 end
