f2fc4978da05a83bc0745ab6ab58876fea91f73c
SHERRY <swasieko@gmail.com>
2017-01-10 07:21:01 +0000

    removed registration functionality from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   # The priority is based upon order of creation: first created -> highest priority.
 
 root to: 'static#homepage'
